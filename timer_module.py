1bda7da705f98f931669cb9cb5fe6b13f5d17007
Ariel Riello <117946691+ArielRiello@users.noreply.github.com>
2023-10-18 14:15:10 -0300

    Update timer_module.py

--- timer_module.py
+++ timer_module.py
@@ -29,17 +29,21 @@ class Timer:
     def obter_informacoes_tempo(self):
         tempo_decorrido = self.tempo_decorrido()
         microticks_decorridos = int(tempo_decorrido * microticks_por_tick)
-        ticks_decorridos = microticks_decorridos // microticks_por_tick
+        ticks_decorridos_total = microticks_decorridos // microticks_por_tick
         microtick_atual = microticks_decorridos % microticks_por_tick
-        dias_passados = ticks_decorridos // ticks_por_dia
+        dias_passados = ticks_decorridos_total // ticks_por_dia
+        
+        # Aqui, fazemos o ajuste para que os ticks sejam zerados a cada dia
+        ticks_decorridos = ticks_decorridos_total % ticks_por_dia
 
         return {
             'tempo_decorrido': tempo_decorrido,
             'microticks_decorridos': microticks_decorridos,
-            'ticks_decorridos': ticks_decorridos,
+            'ticks_decorridos': ticks_decorridos,  # ticks ajustados
             'microtick_atual': microtick_atual,
             'dias_passados': dias_passados
         }
+
 
     def reiniciar(self):
         self.tempo_inicial = time.time()
